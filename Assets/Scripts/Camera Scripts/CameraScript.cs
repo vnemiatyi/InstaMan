dba8915735dcb3f3635be5281858426eb09db017
vnemiatyi <v.nemiatyi@plarium.com>
2019-05-04 16:56:08 +0300

    Make Camera follows Player.

--- Assets/Scripts/Camera Scripts/CameraScript.cs
+++ Assets/Scripts/Camera Scripts/CameraScript.cs
@@ -1,6 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+public enum Mode
+{
+	Easy = 0,
+	Medium = 1,
+	Hard = 2,
+}
+
 public class CameraScript : MonoBehaviour {
 
 	private float speed = 1.0f;
@@ -13,6 +21,11 @@ public class CameraScript : MonoBehaviour {
 	private float easySpeed = 3.2f;
 	private float mediumSpeed = 3.7f;
 	private float hardSpeed = 4.2f;
+
+	[SerializeField]
+	private Transform _player;
+
+	private int _mode = 0;
 	
 	void Start () {
 
@@ -31,19 +44,21 @@ public class CameraScript : MonoBehaviour {
 		moveCamera = true;
 	}
 
-	void Update () {
-		if (moveCamera) {
-			MoveCamera ();	
+	void Update ()
+	{
+		if (moveCamera) 
+		{
+			MoveCamera (_mode);	
 		}
 	}
 
-	void MoveCamera() {
+	void MoveCamera(int mode) {
 		
-		Vector3 temp = transform.position;
+		var temp = transform.position;
 		
-		float oldY = temp.y;
+		var oldY = temp.y;
 		
-		float newY = temp.y - (speed * Time.deltaTime);
+		var newY = NewCameraY(temp.y);
 		
 		temp.y = Mathf.Clamp (temp.y, oldY, newY);
 		
@@ -56,6 +71,20 @@ public class CameraScript : MonoBehaviour {
 		
 	}
 
+	float NewCameraY(float posY)
+	{
+		switch (_mode)
+		{
+			case (int) Mode.Easy:
+				return _player.position.y;
+			case (int) Mode.Medium:
+				return _player.position.y;
+			case (int) Mode.Hard:
+				return posY - (speed * Time.deltaTime);
+			default:
+				return _player.position.y;
+		}
+	}
 } // CameraScript
 
 
