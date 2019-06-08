ed1346d29fc4b2bb671fb9d922c7e9f0b83c75d0
Alex <voyevudko.o@gmail.com>
2019-06-08 18:56:31 +0300

    Add Mana bar and insta

---
+++ Assets/Scripts/Instagram/InstaData.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pagination
+{
+}
+
+public class User
+{
+    public string id;
+    public string full_name;
+    public string profile_picture;
+    public string username;
+}
+
+public class Thumbnail
+{
+    public int width;
+    public int height;
+    public string url;
+}
+
+public class LowResolution
+{
+    public int width;
+    public int height;
+    public string url;
+}
+
+public class StandardResolution
+{
+    public int width;
+    public int height;
+    public string url;
+}
+
+public class Images
+{
+    public Thumbnail thumbnail;
+    public LowResolution low_resolution;
+    public StandardResolution standard_resolution;
+}
+
+public class From
+{
+    public string id;
+    public string full_name;
+    public string profile_picture;
+    public string username;
+}
+
+public class Caption
+{
+    public string id;
+    public string text;
+    public string created_time;
+    public From from;
+}
+
+[System.Serializable]
+public class Likes
+{
+    public int count;
+}
+
+public class Comments
+{
+    public int count;
+}
+
+[System.Serializable]
+public class Datum
+{
+    public string id;
+    public User user;
+    public Images images;
+    public string created_time;
+    public Caption caption;
+    public bool user_has_liked;
+    public Likes likes;
+    public List<object> tags;
+    public string filter;
+    public Comments comments;
+    public string type;
+    public string link;
+    public object location;
+    public object attribution;
+    public List<object> users_in_photo;
+}
+
+public class Meta
+{
+    public int code;
+}
+
+[System.Serializable]
+public class RootObject
+{
+    public Pagination pagination;
+    public List<Datum> data;
+    public Meta meta;
+
+    public static RootObject CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<RootObject>(jsonString);
+    }
+}
+
---
+++ Assets/Scripts/Instagram/InstagramManager.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class InstagramManager : MonoBehaviour
+{
+
+    //Config
+    [SerializeField]
+    private string apiToken;
+
+    [SerializeField]
+    private string postId;
+
+
+    [SerializeField]
+    public float likesCounter, currentLikesCounter, maxLikes, manaPoints, newLikesCounter, likeValue;
+
+    private string URL = "https://api.instagram.com/v1/users/self/media/recent/?access_token=";
+
+    private  int cnt = 0;
+    [SerializeField]
+    private int step = 500;
+
+    [SerializeField]
+    private Slider manabar;
+
+    private void Awake()
+    {
+        StartCoroutine(GetRequest(URL + apiToken));
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(newLikesCounter);
+;        if (newLikesCounter > 0) {
+            if (Input.GetKeyUp(KeyCode.F))
+            {
+                newLikesCounter -= manaPoints;
+                Debug.Log(newLikesCounter);
+                manabar.value = newLikesCounter;
+            }
+        } else if (newLikesCounter <= 0) {
+            ++cnt;
+
+            if (cnt >= step)
+            {
+                Debug.Log("Calling API...");
+                StartCoroutine(GetLikesOnUpdate(URL + apiToken));
+                cnt = 0;
+            }
+        }
+
+        //++cnt;
+        ////Debug.Log(likesCounter);
+        //if (cnt >= step) {
+        //    StartCoroutine(GetLikesOnUpdate(URL + apiToken));
+        //    Debug.Log("Call!!!!");
+        //    cnt = 0;
+        //}
+    }
+
+
+
+    // Calls Instagram API and gets data of recent posts
+    private IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+            else
+            {
+                //Debug.Log(webRequest.downloadHandler.text);
+                var ResponseObj = RootObject.CreateFromJSON(webRequest.downloadHandler.text);
+                var responseData = ResponseObj.data;
+                FindLikesById(responseData);
+            }
+        }
+    }
+
+
+    // Iterate through List to get likes
+    private void FindLikesById(List<Datum> posts)
+    {
+        foreach (Datum post in posts)
+        {
+            if (post.id == postId)
+            {
+                int likes = post.likes.count;
+                likesCounter = Mathf.Round(likes);
+                maxLikes = likesCounter;
+                newLikesCounter = likesCounter / maxLikes;
+                manabar.value = newLikesCounter;
+            }
+        }
+    }
+
+
+    //===========================================================================================
+
+  
+   
+    // Calls Instagram API and gets data of recent posts
+    private IEnumerator GetLikesOnUpdate(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log("Success!");
+                var ResponseObj = RootObject.CreateFromJSON(webRequest.downloadHandler.text);
+                var responseData = ResponseObj.data;
+                FilterCurrentLikes(responseData);
+            }
+        }
+    }
+
+
+    // Iterate through List to get likes
+    private void FilterCurrentLikes(List<Datum> posts)
+    {
+        foreach (Datum post in posts)
+        {
+            if (post.id == postId)
+            {
+                int likes = post.likes.count;
+                currentLikesCounter = Mathf.Round(likes);
+                newLikesCounter = (currentLikesCounter - likesCounter) * likeValue;
+                Debug.Log(newLikesCounter);
+            }
+        }
+    }
+
+    private void DecreaseLikes() {      
+        newLikesCounter -= manaPoints;
+        Debug.Log(newLikesCounter);
+        manabar.value = newLikesCounter;
+    }
+}
+
+
+
+
+
