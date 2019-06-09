6a6750a6304122f5d54a24a88845ea000e18c450
vnemiatyi <v.nemiatyi@plarium.com>
2019-06-09 07:10:39 +0300

    Correct trap action.

--- Assets/Scripts/Player Scripts/TrapDeactivator.cs
+++ Assets/Scripts/Player Scripts/TrapDeactivator.cs
@@ -7,8 +7,6 @@ namespace Player_Scripts
   public class TrapDeactivator : MonoBehaviour
   {
     [SerializeField] private Transform _player;
-    [SerializeField] private BoxCollider2D _platformBoxCollider;
-    [SerializeField] private Transform _platformPosition;
     
     [HideInInspector] private AudioSource _audioSource;
     [HideInInspector] private bool _isTrapAcive = true;
@@ -28,27 +26,12 @@ namespace Player_Scripts
 
         _audioSource.Play();
         
-        ChangePlatform();
-        
-        StartCoroutine(ChangePlayer());
+        ChangePlayer();
       }
     }
 
-    void ChangePlatform()
+    void ChangePlayer()
     {
-      var platformSize = _platformBoxCollider.size;
-      platformSize.y *= 10;
-      _platformBoxCollider.size = platformSize;
-
-      var platformPosition = _platformPosition.localPosition;
-      platformPosition.y = 0;
-      _platformPosition.localPosition = platformPosition;
-    }
-
-    IEnumerator ChangePlayer()
-    {
-      yield return new WaitForEndOfFrame();
-      
       var playerPosition = _player.localPosition;
       playerPosition.y += 0.3f;
       _player.localPosition = playerPosition;
